d11b8fbff7ac4471c347485809312a3da8d94443
RamCodeZ3 <arammusset7@gmail.com>
2025-08-08 11:32:36 -0400

    se realizo la clase1 del modulo 3 con exito.

---
+++ Modulo3/clase1.py
@@ -0,0 +1,13 @@
+cadena = "python es increible"
+caracter = cadena[10:14]
+print(caracter)
+
+caracter2 = cadena[0:10:4]
+print(caracter2)
+
+reversa = cadena[::-1]
+print(reversa)
+
+lenguaje_programacion = "Python C++ C# Javascript Java Go Lua"
+array = lenguaje_programacion.split(' ')
+print(array)
