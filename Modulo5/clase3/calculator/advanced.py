8fabd8527dfcf0ad369ba5d766a617443ec50af0
RamCodeZ3 <arammusset7@gmail.com>
2025-08-23 11:06:25 -0400

    Se realizo practica de la clase 3 del modulo 5 del curso

---
+++ Modulo5/clase3/calculator/advanced.py
@@ -0,0 +1,13 @@
+import math
+
+
+def square_root(num):
+    return math.sqrt(num)
+
+
+def cube_root(num):
+    return num ** (1/3)
+
+
+def rounding(num):
+    return round(num)
---
+++ Modulo5/clase3/main.py
@@ -0,0 +1,6 @@
+from calculator.basic import division
+from calculator.advanced import rounding
+
+d = division(20, 3)
+
+print(rounding(d))
